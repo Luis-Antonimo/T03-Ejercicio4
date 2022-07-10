6fef1306f22c36f76a85eeec6e308e5ad6f58687
Luis Antónimo <luisantonimo@gmail.com>
2022-07-10 23:24:30 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T03Ejercicio4
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 10;
+            Console.WriteLine("N + 77 = {0}", n = n + 77);
+            Console.WriteLine("N - 3 = {0}", n = n - 3);
+            Console.WriteLine("N * 2 = {0}", n = n * 2);
+        }
+    }
+}
